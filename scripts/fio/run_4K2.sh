29d2a265b61681a60ab39f5632053886e71345be
OptimusPrime1997 <874855266@qq.com>
2019-07-22 00:41:24 +0800

    modify run_4K2.sh

--- scripts/fio/run_4K2.sh
+++ scripts/fio/run_4K2.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 R=$1
 S=$2
-C=$3
 Q=$4
 N=$5
 V=$6
-F=$7
-core=$8
+T=$7
 
-fio /home/ljh/exp/scripts/fio/$R > "/home/ljh/exp/logs/fio/4K2_s"$S"_c"$C"_q"$Q"_"$N"_v"$V"_f"$F"_C"$core".log"
+fio /home/ljh/exp/scripts/fio/$R > "/home/ljh/exp/logs/fio/4K2_s"$S"_q"$Q"_"$N"_v"$V"_"$T".log"
 
