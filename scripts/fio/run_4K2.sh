1ba18e3ec446c47c3e712bd02735d39e1a724a4d
OptimusPrime1997 <874855266@qq.com>
2019-07-24 20:24:44 +0800

    add blk log files

--- scripts/fio/run_4K2.sh
+++ scripts/fio/run_4K2.sh
@@ -7,9 +7,9 @@ N=$5
 V=$6
 T=$7
 
-dir="/home/ljh/exp/logs/fio/"$D
+dir="/home/ljh/exp/logs/fio/blk/"$D
 if [ ! -d $dir ];then
 	mkdir $dir
 fi
-fio /home/ljh/exp/scripts/fio/$R > "/home/ljh/exp/logs/fio/"$D"/4K2_s"$S"_q"$Q"_"$N"_v"$V"_"$T".log"
+fio /home/ljh/exp/scripts/fio/$R > $dir"/4K2_s"$S"_q"$Q"_"$N"_v"$V"_"$T".log"
 
