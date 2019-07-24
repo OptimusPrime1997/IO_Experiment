703e2b05ca0847b98d83618f22f1356636b01c70
OptimusPrime1997 <874855266@qq.com>
2019-07-24 14:12:02 +0800

    add run_4K2.sh

--- scripts/fio/run_4K2.sh
+++ scripts/fio/run_4K2.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-R=$1
-S=$2
-Q=$3
-N=$4
-V=$5
-T=$6
+D=$1
+R=$2
+S=$3
+Q=$4
+N=$5
+V=$6
+T=$7
 
-fio /home/ljh/exp/scripts/fio/$R > "/home/ljh/exp/logs/fio/4K2_s"$S"_q"$Q"_"$N"_v"$V"_"$T".log"
+dir="/home/ljh/exp/logs/fio/"$D
+if [ ! -d $dir ];then
+	mkdir $dir
+fi
+fio /home/ljh/exp/scripts/fio/$R > "/home/ljh/exp/logs/fio/"$D"/4K2_s"$S"_q"$Q"_"$N"_v"$V"_"$T".log"
 
