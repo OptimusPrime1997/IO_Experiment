d8c502d05e8eabe587235b3087a9d8ddf6d4f9a3
OptimusPrime1997 <874855266@qq.com>
2019-08-07 00:41:48 +0800

    change script

--- scripts/tpcc-mysql/tpcc_execute_C.sh
+++ scripts/tpcc-mysql/tpcc_execute_C.sh
@@ -49,7 +49,7 @@ do
 	done
         echo $data
 #	sudo bash /home/ljh/exp/scripts/tpcc-mysql/restart.sh > /dev/null 2>&1 
-	sudo nice -n -19 taskset -c ${CPU}-$ALL  /home/ljh/projects/tpcc-mysql/tpcc_start -h $IP -P $PORT -dbenchmarker -uroot -p123456 -w11 -c${data} -r22 -l60 -i10 > $dir"tpcc_"${num}_$d".log"   2>&1
+	sudo nice -n -20 taskset -c ${CPU}-$ALL  /home/ljh/projects/tpcc-mysql/tpcc_start -h $IP -P $PORT -dbenchmarker -uroot -p123456 -w11 -c${data} -r22 -l60 -i10 > $dir"tpcc_"${num}_$d".log"   2>&1
         #sudo sysbench /usr/share/sysbench/oltp_read_write.lua --threads=$data --events=0 --time=60 --mysql-host=127.0.0.1 --mysql-user=root --mysql-password=123456 --mysql-port=13306 --db-driver=mysql --tables=10 --table-size=1000000 --range_selects=off --db-ps-mode=disable --report-interval=10  run  > /var/log/sysbench/thread_${op}${data}_$d.log
 	i=$[i+1]
 done
